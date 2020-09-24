f890b7a06365cdb6ec347354dc5955e3adff1bfa
RazzleJazzled <robertjgassner@gmail.com>
2020-09-23 17:27:40 -0700

    file name change

---
+++ models/resistance.js
@@ -0,0 +1,29 @@
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ResistanceSchema = new Schema({
+    type: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    duration: {
+        type: Number
+    }, 
+    weight: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    sets: {
+        type: Number
+    },
+});
+
+const Resistance = mongoose.model("Resistance", ResistanceSchema);
+
+module.exports = Reistance; 
