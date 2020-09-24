431d188671c4915041c906fdf34428d9478a1f51
RazzleJazzled <robertjgassner@gmail.com>
2020-09-23 17:03:50 -0700

    basic server setup

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3030;
 
+const db = require("./models");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {useNewUrlParser: true});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
