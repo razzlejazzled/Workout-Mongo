d0759520ad20e196bad305057123eeea0bba4e22
RazzleJazzled <robertjgassner@gmail.com>
2020-09-25 16:38:20 -0700

    server config

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const PORT = process.env.PORT || 3030;
 
-const db = require("./models");
+
+// const db = require("./models");
 
 const app = express();
+const PORT = process.env.PORT || 3030;
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(morgan("dev"));
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
 
-require("./routes/html-routes.js")(app)
-require("./routes-api-routes.js")(app)
+// require("./routes/html-routes.js")(app)
+// require("./routes-api-routes.js")(app)
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
 });
 
-app.get("/")
