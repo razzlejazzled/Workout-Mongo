f29be91e6c9aa930596c6baf8ad7078d0040ce2f
RazzleJazzled <robertjgassner@gmail.com>
2020-09-25 16:45:28 -0700

    added useUnifiedTopology

--- server.js
+++ server.js
@@ -8,16 +8,18 @@ const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
-
-// require("./routes/html-routes.js")(app)
-// require("./routes-api-routes.js")(app)
+app.use(require("./routes"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
