02b4057bf346ebc5b1d2471c2d3a77501eb176d3
RazzleJazzled <robertjgassner@gmail.com>
2020-09-25 17:29:48 -0700

    basic model  finished

--- models/Workout.js
+++ models/Workout.js
@@ -1,16 +1,46 @@
-let mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkOutSchema = new Schema({
-  type: String,
-  name: String,
-  duration: Number,
-  weight: Number,
-  reps: Number,
-  sets: Number,
-})
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  excersies: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "type of exercise is required"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "name of exercise is required"
+      },
+      duration: {
+        type: Number,
+        required: "Duration is required"
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
 
-const WorkOut = mongoose.model("WorkOut", WorkOutSchema)
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.exports = WorkOut
+
+
+module.exports = Workout
