0aa3aa1463f83ad201d725f328caf75c15e6f2cc
RazzleJazzled <robertjgassner@gmail.com>
2020-09-25 17:56:15 -0700

    post and put routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,4 +6,12 @@ app.get("/workouts", async function(req, res){
     res.json(await db.Workout.find());
 });
 
+app.post("/workouts", async function(req, res){
+    res.json(await db.Workout.create(req.body));
+});
+
+app.put("/workouts/:id", async function (req, res) {
+    res.json(await db.Workout.findByIdAndUpdate(req.params.id, req.body));
+})
+
 module.exports = app;
