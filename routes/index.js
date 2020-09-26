16d8341378aa587de00dd4f7130949e3386b1f34
RazzleJazzled <robertjgassner@gmail.com>
2020-09-25 17:52:38 -0700

    require path

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
+const path = require("path");
 const app = require("express").Router()
 
+const publicDir = path.resolve(__dirname, "../public");
 app.use("/api", require("./api"));
+
+app.get("/stats", function (req, res) {
+    res.sendFile(path.resolve(publicDir, "stats.html"));
+});
 
 module.exports = app;
