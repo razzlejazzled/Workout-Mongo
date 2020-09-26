e44e77e290d6b4eb3368feba71add2669ce7f234
RazzleJazzled <robertjgassner@gmail.com>
2020-09-25 17:44:10 -0700

    added changes

--- foo.js
+++ foo.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
 
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
+});
+
+const db = require ("./models");
+
+db.Workout.find().then(dbWorkouts => {
+   const workout = dbWorkouts[0];
+   console.log(workout);
+  
 });
